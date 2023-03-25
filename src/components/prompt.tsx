b9204333c32ec31fa6df32b9e32c75af987035d5
javier <jpesgil@gmail.com>
2023-03-25 12:31:34 +0000

    feat: voice recognition

--- src/components/prompt.tsx
+++ src/components/prompt.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useRef} from 'react'
 import {useConversationStore} from '@/stores/conversation';
 import {Loading} from './loading';
-import {SendIcon} from './icons';
+import {MicrophoneIcon, SendIcon} from './icons';
 import {toast} from 'sonner';
 
 export const Prompt = () => {
@@ -16,8 +16,8 @@ export const Prompt = () => {
             el.focus()
         }
     }, [])
-    useEffect(()=>{
-        if(error){
+    useEffect(() => {
+        if (error) {
             toast.error(error)
         }
     }, [error])
@@ -40,8 +40,38 @@ export const Prompt = () => {
             const scrollHeight = el.scrollHeight
             el.style.height = `${scrollHeight}px`
         }
-
     }
+
+    function handleSpeechToText() {
+        const SpeechRecognition =
+            window.SpeechRecognition || window.webkitSpeechRecognition
+
+        if (!SpeechRecognition) {
+            toast.error('Speech Recognition is not supported by your browser')
+            return
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const area = textAreaRef.current!
+        area.value = ''
+
+
+        const recognition = new SpeechRecognition()
+        recognition.interimResults = true
+        recognition.lang = 'es-ES'
+
+        recognition.addEventListener('result', (e) => {
+            area.value = Array.from(e.results)
+                .map((result) => result[0])
+                .map((result) => result.transcript)
+                .join('')
+        })
+
+        recognition.addEventListener('end', handleSubmit)
+
+        recognition.start()
+    }
+
     return (
         <form onSubmit={handleSubmit}>
             <div className="relative"> 
@@ -67,9 +97,15 @@ export const Prompt = () => {
                 />
                 <div className="absolute right-4 top-0 flex justify-center h-full items-center">
                     {streaming ? <Loading/> : (
-                        <button className="hover:scale-125 transition-all" type="submit">
-                            <SendIcon/>
-                        </button>
+                        <div className="flex items-center gap-2">
+                            <button className="transition-all hover:scale-125" type="button"
+                                    onClick={handleSpeechToText}>
+                                <MicrophoneIcon/>
+                            </button>
+                            <button className="hover:scale-125 transition-all" type="submit">
+                                <SendIcon/>
+                            </button>
+                        </div>
                     )}
                 </div>
             </div>
