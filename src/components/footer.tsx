4cc6e747d2a740aeb570ef3ce7b838822bc291e8
javier <jpesgil@gmail.com>
2023-03-25 12:02:56 +0000

    fix: styles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     const code = useConversationStore(state => state.code)
     return (
         <footer
-            className={`fixed bottom-0 left-0 right-0 block pb-20 mt-10 text-center animate-delay-1000 opacity-60 text-white/80 ${
+            className={`bottom-0 left-0 right-0 block pb-20 mt-10 text-center animate-delay-1000 opacity-60 text-white/80 ${
                 code ? 'animate-fadeOut' : 'animate-delay-500 animate-fadeIn'
             }`}
         >
